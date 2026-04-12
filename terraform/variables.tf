0227818a2b7e108b9904a4dbf04e437be3adcc6f
Jeff Bryner <jeff@jeffbryner.com>
2026-04-12 10:35:06 -0700

    set pubsub topic

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,16 +12,17 @@ variable "region" {
 variable "pubsub_topic_name" {
   description = "The name of the existing Pub/Sub topic to publish events to"
   type        = string
+  default     = "defenda-event-ingest"
 }
 
 variable "collectors" {
   description = "A map of collector configurations to deploy"
   type = map(object({
-    enabled          = bool
-    image            = string
-    schedule         = string
-    env_vars         = map(string)
-    secret_env_vars  = map(string)
+    enabled         = bool
+    image           = string
+    schedule        = string
+    env_vars        = map(string)
+    secret_env_vars = map(string)
   }))
   default = {}
 }
